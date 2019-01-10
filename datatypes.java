aefa6db5c14cc0d45b4f621f35be99fb6600be10
rubal18csu180 <rubal18csu180@ncuindia.edu>
2019-01-10 20:35:29 +0530

    done

---
+++ datatypes.java
@@ -0,0 +1,16 @@
+class datatypes
+{
+	public static void main(String args[])
+	{
+		System.out.println("s.no. datatype size minvalue maxvalue
+			1 long 64 -9 807
+			2 int 32 -2 647
+			3 short 16 -32 767
+			4 byte -128 127
+			5 double 64 4.9e-324 1.8e+308
+			6 float 32 1.4e-045 3.4e+038
+			7 char 16 0 65536");
+		
+	}
+}
+
