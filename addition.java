17f1191a475c522d96f3fad6473a677e2e198c71
rubal18csu180 <rubal18csu180@ncuindia.edu>
2019-01-11 14:16:04 +0530

    done

---
+++ addition.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class addition
+{
+	public static void main(String args[])
+	{
+		Scanner sc =new Scanner(System.in);
+		System.out.println("type 2 no.s");
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		int add=n1+n2;
+		System.out.println("addition is"+add);
+}
+}
