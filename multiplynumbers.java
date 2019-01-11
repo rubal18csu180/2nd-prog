28dca3b4a89b0deefea016dbf3438d00145b1de5
rubal18csu180 <rubal18csu180@ncuindia.edu>
2019-01-11 14:03:33 +0530

    done

---
+++ multiplynumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class multiplynumbers
+{
+	public static void main(String args[])
+	{
+		Scanner sc =new Scanner(System.in);
+		System.out.println("type 2 no.s");
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		int pro=n1*n2;
+		System.out.println("product is"+pro);
+}
+}
